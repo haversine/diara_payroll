c92b9bc6fad6b703e2c90e6d2f2c4e80320f9784
Madis Kase <madiskase@hotmail.com>
2013-02-04 23:58:34 +0200

    Added user model and login controller

---
+++ application/classes/Controller/Login.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: madis
+ * Date: 2/4/13
+ * Time: 11:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Controller_Login
+{
+
+}
